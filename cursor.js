8daeb344f62d2d500e206687282ce31cf67f7a5b
caio-lcf <cardosocaio032@gmail.com>
2024-04-29 17:31:43 -0300

    finalizado!

--- cursor.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("mousemove", function(e) {
-    var cursor = document.querySelector(".cursor");
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
-  });
-  
